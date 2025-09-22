051acfced307fd4e90a9cf2c7423f297f01379b2
BoredDany <martinezq.d@javeriana.edu.co>
2025-09-21 21:06:30 -0500

    fixed kafka message

--- app/application/dto/practice_data_dto.py
+++ app/application/dto/practice_data_dto.py
@@ -4,7 +4,12 @@ from dataclasses import dataclass
 class PracticeDataDTO:
     uid: int
     practice_id: int
+    date: str
+    time: str
     scale: str
     scale_type: str
-    reps: str
+    num_postural_errors: int
+    num_musical_errors: int
+    duration: int
+    reps: int
     bpm: int
